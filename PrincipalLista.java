315b6f96e612de213e5412e57a1f96d3f12c6575
Asking Martin <DeipMartin@users.noreply.github.com>
2016-01-22 01:29:37 -0600

    Create PrincipalLista.java

---
+++ PrincipalLista.java
@@ -0,0 +1,17 @@
+package ListaSimple;
+public class PrincipalLista{
+	public static void main(String[]args){
+		Lista 1= new Lista();
+		l.InsertarInicio("a");
+		l.InsertarFinal("b");
+		l.InsertarFinal("c");
+		l.InsertarFinal("d");
+		l.InsertarFinal("f");
+		l.InsertarFinal("e");
+		l.EliminarInicio();
+		l.Listar();
+		
+		
+
+	}
+}
