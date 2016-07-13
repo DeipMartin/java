bc2e0badab9830a0912cd47d85c87b93b377bc37
Leon Martin <DeipMartin@users.noreply.github.com>
2016-07-13 17:33:36 -0500

    Create Loops.java

---
+++ Loops.java
@@ -0,0 +1,16 @@
+public class Loops {
+    public static void main(String[] args) {
+        int count=100,variable=count;
+        final boolean cound = false;
+        /*for(; count >10; variable++){
+            System.out.println("ciclo "+(variable*variable));
+        }*/
+        for( generarPassword();generarPassword(); count++ ){
+            System.out.println("Ciclo" + count);
+            class perro{ int a = 22; public void main2(String[] args){} }
+        }        
+    }
+    public static boolean generarPassword(){
+            return true;
+        }
+}
