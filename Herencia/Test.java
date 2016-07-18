20ac72d9368468ff4d6e64e7f1f46b3c87fc97d4
Leon Martin <DeipMartin@users.noreply.github.com>
2016-07-18 16:10:58 -0500

    Create Test.java

---
+++ Herencia/Test.java
@@ -0,0 +1,14 @@
+package mx.com.develop.employees;
+import mx.com.develop.test.Manager;
+
+public class TestEmploye {
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        Manager m1 = new Manager();
+        e1.firstName="ricardo";
+        e1.lastName="gomo";
+        m1.firstName="ricardo";
+        m1.lastName="yolo";
+          
+    }
+}
