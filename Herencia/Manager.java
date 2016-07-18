c8fbc75010a4c60e70bd56c0a056d9162d01b15f
Leon Martin <DeipMartin@users.noreply.github.com>
2016-07-18 17:57:57 -0500

    Update Manager.java

--- Herencia/Manager.java
+++ Herencia/Manager.java
@@ -1,27 +1,45 @@
-package mx.com.develop.test;
+package mx.com.develop.employees;
 
-import mx.com.develop.employees.Employee;
-import mx.com.develop.employees.Employee;
+public class Employee {
+    protected int id;
+    protected String firstName;
+    protected String lastName;
+    protected String mail;
+    protected int age;
 
-public class Manager extends Employee {
-  public String department;
-  public int getDepartment(){
-    return id;
+    public int getId(){
+      return id;
+    }
+    public String getFirstName(){
+      return firstName;
+    }
+    public void setFirstName(){
+      this.firstName = firstName;
+    }
+    public String getLastName(){
+      return lastName;
+    }
+    public void setLastName(){
+      this.lastName = lastName;
+    }
+    public String getMail(){
+      return mail;
+    }
+    public void setMail(){
+      this.mail = mail;
+    }
+    public int getAge(){
+      return age;
+    }
+    public void setAge(){
+      this.age = age;
+    }
+    public String getDetails(){
+        return id + " "+ firstName+""+lastName;
+    }
+    
+    public Object getThis(){
+      return null;
   }
-  public void setDepartment(){
-    this.department = department;
-    this.firstName = "Edgar";
-   // mal hecho -> Employee e = new Employee();
-    Manager e = new Manager();
-    e.firstName = "Edgar";
-  }
-  @Override
-  public String getDetails(){
-      return age + " "+ firstName+" departeamento: "+ department +" "+lastName;
-  }
-  @Override
-  public Manager getThis(){
-      return this;
-  }
-
 }
+
