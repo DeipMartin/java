0a0487e00d04e15cd20bf33e7a04c53d2d965e00
Leon Martin <DeipMartin@users.noreply.github.com>
2016-07-18 16:09:25 -0500

    Create Employee.java

---
+++ Herencia/Employee.java
@@ -0,0 +1,41 @@
+package mx.com.develop.employees;
+
+public class Employee {
+    protected int id;
+    protected String firstName;
+    protected String lastName;
+    protected String mail;
+    protected int age;
+
+    public int getId(){
+      return id;
+    }
+    public String getFirstName(){
+      return firstName;
+    }
+    public void setFirstName(){
+      this.firstName = firstName;
+    }
+    public String getLastName(){
+      return lastName;
+    }
+    public void setLastName(){
+      this.lastName = lastName;
+    }
+    public String getMail(){
+      return mail;
+    }
+    public void setMail(){
+      this.mail = mail;
+    }
+    public int getAge(){
+      return age;
+    }
+    public void setAge(){
+      this.age = age;
+    }
+    public String getDetails(){
+        return id + " "+ firstName+""+lastName;
+    }
+}
+
