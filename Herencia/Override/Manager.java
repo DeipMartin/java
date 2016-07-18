a1a1e8e4c3e7de03ba13da776dc9a1d2ca540d7c
Leon Martin <DeipMartin@users.noreply.github.com>
2016-07-18 16:52:25 -0500

    Create Manager.java

---
+++ Herencia/Override/Manager.java
@@ -0,0 +1,27 @@
+package mx.com.develop.test;
+
+import mx.com.develop.employees.Employee;
+import mx.com.develop.employees.Employee;
+
+public class Manager extends Employee {
+  public String department;
+  public int getDepartment(){
+    return id;
+  }
+  public void setDepartment(){
+    this.department = department;
+    this.firstName = "Edgar";
+   // mal hecho -> Employee e = new Employee();
+    Manager e = new Manager();
+    e.firstName = "Edgar";
+  }
+  @Override
+  public String getDetails(){
+      return age + " "+ firstName+""+age +" "+lastName;
+  }
+  @Override
+  public Manager getThis(){
+      return this;
+  }
+
+}
