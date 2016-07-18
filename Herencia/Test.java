cc1f29a3a6a5fc78c993587861271e293caf87aa
Leon Martin <DeipMartin@users.noreply.github.com>
2016-07-18 17:13:23 -0500

    Update Test.java

--- Herencia/Test.java
+++ Herencia/Test.java
@@ -1,14 +1,29 @@
 package mx.com.develop.employees;
-import mx.com.develop.test.Manager;
 
+
+import mx.com.develop.test.Manager;
 public class TestEmploye {
     public static void main(String[] args) {
         Employee e1 = new Employee();
         Manager m1 = new Manager();
         e1.firstName="ricardo";
         e1.lastName="gomo";
+        e1.mail="hola@gmail";
+        e1.id=(23);
         m1.firstName="ricardo";
         m1.lastName="yolo";
-          
+        m1.mail="hola@gmail";
+        m1.id=(23);
+        
+        //poliformismo
+        Employee e2 = new Employee();
+        e2 = new Manager();
+        e2.age=27;
+        e2.mail="fsdfsdffsdf@gmail";
+        e2.id=23213;
+        e2.firstName="diana";
+        e2.lastName="gutierrez";
+        //invocacion virtual de metodos!! 
+        System.out.println(e2.getDetails());
     }
 }
