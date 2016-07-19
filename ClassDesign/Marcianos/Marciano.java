608776f0830d5d3574129b0cee0ab6fd6a04ddf0
Leon Martin <DeipMartin@users.noreply.github.com>
2016-07-19 18:02:43 -0500

    Create Marciano.java

---
+++ ClassDesign/Marcianos/Marciano.java
@@ -0,0 +1,15 @@
+
+package Static;
+
+public class Marciano {
+    public static int cuentaMarciano = 12;
+
+    public static int getCuentaMarciano() {
+        return cuentaMarciano;
+    }
+
+    public static void setCuentaMarciano(int cuentaMarciano) {
+        Marciano.cuentaMarciano = cuentaMarciano;
+    }
+    
+}
