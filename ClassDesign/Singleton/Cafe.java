9502de79000a8ddbaad7f34cd91c8af47ae10583
Leon Martin <DeipMartin@users.noreply.github.com>
2016-07-20 16:20:46 -0500

    Update Cafe.java

--- ClassDesign/Singleton/Cafe.java
+++ ClassDesign/Singleton/Cafe.java
@@ -1,13 +1,19 @@
+//Ocupar enums cuando ocupamos listas de productos 
 package oxxo.enums;
 // patron de diseño singleton 
 public enum Cafe {
     CHICO(12.90,400),MEDIANO(15.00,600),GRANDE(27.80,900);
     private double precio;
     private double mililitros;
+    
     // los constructores de enum siempre son private
     private Cafe(double precio, double mililitros) {
         this.precio = precio;
         this.mililitros = mililitros;
     }
     
+    @Override
+    public String toString(){
+        return "cafe : "+this.name() + ", precio: "+ precio + ", mililitros: "+mililitros;
+    }
 }
