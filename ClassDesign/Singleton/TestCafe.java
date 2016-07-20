663e553a50ca5f6f36ed42fb6ebf5243e90a22ea
Leon Martin <DeipMartin@users.noreply.github.com>
2016-07-20 16:21:15 -0500

    Update TestCafe.java

--- ClassDesign/Singleton/TestCafe.java
+++ ClassDesign/Singleton/TestCafe.java
@@ -3,5 +3,13 @@ package oxxo.enums;
 public class TestCafe {
     public static void main(String[] args) {
         Cafe americano = Cafe.CHICO;
+        System.out.println(americano);
+        americano = Cafe.GRANDE;
+        System.out.println(americano);
+        System.out.println("-------------------------------------");
+        //for each
+        for(Cafe c:Cafe.values()){
+            System.out.println(c);
+        }
     }
 }
