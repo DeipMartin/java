86477a4b6f008e9ed645f2d12252e155a35104e3
Leon Martin <DeipMartin@users.noreply.github.com>
2016-07-20 16:15:52 -0500

    Create Coffee.java

---
+++ ClassDesign/Singleton/Coffee.java
@@ -0,0 +1,19 @@
+package oxxo;
+
+public class Coffee {
+    public static final int CHICO=0;
+    public static final int MEDIANO=2;
+    public static final int GRANDE=3;
+
+    public static int getCHICO() {
+        return CHICO;
+    }
+
+    public static int getMEDIANO() {
+        return MEDIANO;
+    }
+
+    public static int getGRANDE() {
+        return GRANDE;
+    }
+}
