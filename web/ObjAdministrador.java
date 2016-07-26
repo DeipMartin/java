61a8a155c865cc7445fca74c9a99d860ace5a1ff
Leon Martin <DeipMartin@users.noreply.github.com>
2016-07-26 18:10:46 -0500

    Update ObjAdministrador.java

--- web/ObjAdministrador.java
+++ web/ObjAdministrador.java
@@ -6,4 +6,30 @@ public class ObjAdministrador{
 	private String email;
 	private String password;
 
+    public int getIdAdministrador() {
+        return idAdministrador;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setIdAdministrador(int idAdministrador) {
+        this.idAdministrador = idAdministrador;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+        
+    
+
 }
